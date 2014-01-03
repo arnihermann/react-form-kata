c28403c7481f1f167123465d49f590cd7b614e95
Árni Hermann Reynisson <arnihr@gmail.com>
2014-01-03 11:10:14 +0000

    Add boostrap starter template

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 
 require('../bower_components/bootstrap/dist/css/bootstrap.css');
+require('../bower_components/bootstrap/dist/css/bootstrap-theme.css');
+
+require('file!../bower_components/jquery/jquery.js');
+require('file!../bower_components/bootstrap/dist/js/bootstrap.js');
+
+require('./starter-template.css');
